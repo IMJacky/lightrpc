7c51e78757fe1eac0d8d0bcaf1ec340bc981454b
wjg50058 <jieguang.wang@ly.com>
2021-01-15 11:01:53 +0800

    init

---
+++ src/main/java/com/jiqunar/lightrpc/LightrpcApplication.java
@@ -0,0 +1,13 @@
+package com.jiqunar.lightrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightrpcApplication.class, args);
+    }
+
+}
